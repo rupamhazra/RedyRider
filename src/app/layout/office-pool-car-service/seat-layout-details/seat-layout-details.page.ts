5e7adca5233bfedb6cc6feadec75d23143ef2406
priyam9cd <priyamkumardas@gmail.com>
2020-06-06 14:09:26 +0530

    priyam

--- src/app/layout/office-pool-car-service/seat-layout-details/seat-layout-details.page.ts
+++ src/app/layout/office-pool-car-service/seat-layout-details/seat-layout-details.page.ts
@@ -7,12 +7,18 @@ import { Events } from '@ionic/angular';
 import { OfficePoolCarService } from '../../../core/services/office-pool-car.service';
 import { Storage } from '@ionic/storage';
 
+declare var google;
+
 @Component({
   selector: 'app-seat-layout-details',
   templateUrl: './seat-layout-details.page.html',
   styleUrls: ['./seat-layout-details.page.scss'],
 })
 export class SeatLayoutDetailsPage implements OnInit {
+  ////////priyam distance api////////////
+  distanceService = new google.maps.DistanceMatrixService;
+  /////////////////////////////////////
+
   seatClass: any = 'seatAvailable';
   seatNo: string;
   net_connection_check: boolean = false;
@@ -100,6 +106,7 @@ export class SeatLayoutDetailsPage implements OnInit {
     this.officePoolCarService.commonSearchService(request_data).subscribe(
       res => {
         this.allSeats_round = res.result;
+        this.Distance_api();
       },
       error => {
         this.toasterService.showToast(error.error.msg, 2000, true, false, '', '', 'my-error-toast');
@@ -110,8 +117,10 @@ export class SeatLayoutDetailsPage implements OnInit {
     console.log('request_data', request_data)
     this.officePoolCarService.commonSearchService(request_data).subscribe(
       res => {
+        //console.log('this.allSeats',res.result);
         this.allSeats = res.result;
         this.progress_bar = false;
+        this.Distance_api();
       },
       error => {
         //console.log("error::::" + error.error);
@@ -120,6 +129,49 @@ export class SeatLayoutDetailsPage implements OnInit {
       }
     );
   }
+
+  ////////Priyam Distance api google 05.06.2020/////////////
+
+  Distance_api(){
+
+    let distance;
+    const that =this;
+    let star_lat_lng={lat: parseFloat(this.allSeats.start_point_lat),lng: parseFloat(this.allSeats.start_point_long)};
+    let end_lat_lng={lat: parseFloat(this.allSeats.end_point_lat),lng: parseFloat(this.allSeats.end_point_long)};
+    this.distanceService.getDistanceMatrix({
+        origins: [star_lat_lng],
+        destinations: [end_lat_lng],
+        travelMode: google.maps.TravelMode.DRIVING,
+        unitSystem: google.maps.UnitSystem.METRIC,
+        avoidHighways: false,
+        avoidTolls: false
+      }, function (response, status) {
+        if (status !== 'OK') {
+          alert('Start distance matrix Error was: ' + status);
+        } else {
+          //console.log(response.rows[0].elements[0].distance.text);
+          distance=response.rows[0].elements[0].distance.text;
+
+          that.storage.get('route_search_parameters').then((val) => {
+            let val1 = val;
+            val1['distance'] = distance;
+            
+            that.storage.set('route_search_parameters', val1);
+          })
+    
+
+        }
+      });
+
+      
+  }
+
+
+
+
+  ////////////////////Distance Api end//////////////////////
+
+
   toggleClass(item, car_id) {
     item.selected = !item.selected;
     if (item.selected) {
